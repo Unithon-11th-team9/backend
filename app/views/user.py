120a3f8edda9414bff098db9e833de1487cfbc55
Daco2020 <dacokim32@gmail.com>
2024-04-05 13:14:47 +0900

    feat: 카카오 소셜 로그인 추가

--- app/views/user.py
+++ app/views/user.py
@@ -1,10 +1,36 @@
-from fastapi import APIRouter, Depends
+from typing import Literal
+from fastapi import APIRouter, Body, Depends, Response
 from starlette.status import HTTP_200_OK, HTTP_204_NO_CONTENT
 
 from app import deps, dto
+from app.base.auth import login
+from app.base.provider import KakaoAuthProvider
+from app.exceptions import ValidationError
 from app.services.user import UserService
 
 router = APIRouter()
+
+
+@router.post(
+    "/social-login/{provider}",
+    status_code=HTTP_200_OK,
+    response_model=dto.UserProfile,
+)
+async def social_login(
+    response: Response,
+    provider: Literal["kakao"],
+    token: str = Body(embed=True),
+    user_service: UserService = Depends(deps.user_service),
+) -> dto.UserProfile:
+    """소셜 로그인을 합니다."""
+    if provider == "kakao":
+        kakao_provider = KakaoAuthProvider(token)
+        user_info = kakao_provider.get_user_info()
+    else:
+        raise ValidationError(f"지원하지 않는 소셜 플랫폼입니다. '{provider}'")
+    user = await user_service.get_or_create_user(provider, user_info)
+    login(response, user.id)
+    return user
 
 
 @router.get(
