117b5b01b0012c161a0b1ed20f61b27e5eb1732f
Daco2020 <dacokim32@gmail.com>
2024-04-07 05:25:29 +0900

    chore: https를 위한 임시조치

--- scripts/run-server.sh
+++ scripts/run-server.sh
@@ -5,4 +5,7 @@ set -e
 ARGS=$@
 # poetry install --sync
 pip install -r requirements.txt
-uvicorn app.main:app --host 0.0.0.0 --port 8000 $ARGS
+# uvicorn app.main:app --host 0.0.0.0 --port 8000 $ARGS
+
+# TODO: 삭제 예정
+nohup uvicorn app.main:app --host 0.0.0.0 --port 3390 &
