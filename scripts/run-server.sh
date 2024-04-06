fbd6a7183792cea7d8de5dc208d875aad453e9c2
Daco2020 <dacokim32@gmail.com>
2024-04-07 05:30:13 +0900

    chore: https를 위한 임시조치

--- scripts/run-server.sh
+++ scripts/run-server.sh
@@ -8,4 +8,4 @@ pip install -r requirements.txt
 # uvicorn app.main:app --host 0.0.0.0 --port 8000 $ARGS
 
 # TODO: 삭제 예정
-nohup uvicorn app.main:app --host 0.0.0.0 --port 3390 &
+nohup uvicorn app.main:app --host 0.0.0.0 --port 3389 &
