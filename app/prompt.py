31addef201d4f8a3d6f08d372931b1a1ec95937f
Daco2020 <dacokim32@gmail.com>
2024-04-06 21:26:40 +0900

    chore: 비동기로 변경

--- app/prompt.py
+++ app/prompt.py
@@ -1,19 +1,18 @@
+# from datetime import datetime
 import json
-import os
-import traceback
-from openai import OpenAI
+from openai import AsyncOpenAI
 
-from app.base.utils import send_message
 from app.dto import PeaceAwardOutput
 from app.exceptions import ValidationError
 
 from app.base import config
 
 
-client = OpenAI(api_key=config.OPENAI_API_KEY)
+client = AsyncOpenAI(api_key=config.OPENAI_API_KEY)
 
 
-def get_peace_award(chat_content: str) -> PeaceAwardOutput:
+async def get_peace_award(chat_content: str) -> PeaceAwardOutput:
+    # start_time = datetime.now()
     instructions = """
 - 너는 날짜, 발화자, 문자메시지 내용을 토대로 관계의 평화를 분석하는 장난꾸러기야.
 - 너에게 날짜, 발화자, 문자메시지 내용을 보내줄거야. 내용을 토대로 `평화 점수`와 `전체 대화 내용 요약`, `각 발화자의 mbti 분석`을 해줘. 구체적인 내용은 아래 내용을 참고해.
@@ -23,10 +22,9 @@ def get_peace_award(chat_content: str) -> PeaceAwardOutput:
 - 각 발화자별로 `칭찬/긍정 점수`에 `욕설/부정 점수`를 빼서 `평화 점수`를 계산해줘.
 - `칭찬/긍정 점수`는 발화자가 다른 사람에게 긍정적인 말을 한 점수야.
 - `욕설/부정 점수`는 발화자가 방어적이고 부정적인 말을 한 점수야.
-- `평화 점수`는 정수이고 최대 100점이야.
+- `평화 점수`는 `칭찬/긍정 점수` 에서 `욕설/부정 점수`를 뺀 정수이고 최대 100점이야.
 - 각각 발화자의 평화 점수는 중복 불가야.
-- 평화 점수를 산정할 때, 화자가 서로의 감정을 좋게 만들기 위해 노력했는지를 보아야 해, 예를 들어, 영희가 "우리 모두 힘내자"라고 말하거나, "화이팅이이야"라고 이야기한다면 그런 노력을 시행한 것이겠지?
-그렇다면 평화 점수를 높이 줄 수 있어
+- 평화 점수를 산정할 때, 화자가 서로의 감정을 좋게 만들기 위해 노력했는지를 보아야 해, 예를 들어, 영희가 "우리 모두 힘내자"라고 말하거나, "화이팅이이야"라고 이야기한다면 그런 노력을 시행한 것이겠지? 그렇다면 평화 점수를 높이 줄 수 있어
 - 평화 점수의 분포가 0부터 100점까지 고르게 분포하게 만들어야 해 (모든 참여자가 90점을 넘지 않게 해 줘)
 
 ### `전체 대화 내용 요약`
@@ -53,32 +51,33 @@ def get_peace_award(chat_content: str) -> PeaceAwardOutput:
   예시:
     (Response: {"peace_score": {"홍길동": 83, "장길산": 57, "장득현": 94, "백민후": 59}, "summary": "네 명의 팀원이 각자의 작업 상황과 고민을 공유하고 서로 조언과 격려를 해주는 내용이야", "mbti_analysis": {"E": "홍길동", "I": "장길산", "F": "장득현", "T": "백민후"})
     (Response: {"peace_score": {"백인혁": 73, "김송": 34} "summary": "야~ 이 친구들 목표 달성하려고 콕 집어서 계획 다지고 살피네? 재밌는 일도 하고 서로 밀어주고!", "mbti_analysis": {"E": "백인혁", "I": "김송"})
+    (Response: {"peace_score": {"박지훈": 42, "김재현": 80, "이승훈": 85, "김동현": 87, "이승준": 88, "김동현": 84}, "summary": "엄청 재밌는 일들 많았고, 모두가 진짜 알차게 놀았어~", "mbti_analysis": {"E": "박지훈", "I": "김동현", "F": "이승훈", "T": "김동현"}})
     """
     try:
         # chat_content = get_chat_content() # 테스트용 코드입니다.
-        response = client.chat.completions.create(
+        response = await client.chat.completions.create(
             # model="gpt-3.5-turbo-16k",
             model="gpt-4-turbo-preview",
             messages=[
                 {"role": "system", "content": instructions},
                 {
                     "role": "user",
-                    "content": chat_content[-15000:],
-                },  # TODO: 15000자 이상은 분리 필요
+                    "content": chat_content[-10000:],
+                },  # TODO: 속도를 위해 최근 10000자 미만으로 요청
             ],
             n=5,
             response_format={"type": "json_object"},
         )
         summaries = [
-            json.loads(choice.message.content)["summary"]
-            for choice in response.choices[1:]
+            json.loads(choice.message.content)["summary"] for choice in response.choices
         ]
         content = json.loads(response.choices[0].message.content)
         content["summary"] = summaries
-        return PeaceAwardOutput(**content)
-    except Exception:
-        send_message(str(traceback.format_exc()))
-        raise ValidationError("대화내용 분석 중에 오류가 발생했습니다.")
+        res = PeaceAwardOutput(**content)
+        # print(f"실행 시간: {datetime.now() - start_time}")
+        return res
+    except Exception as e:
+        raise ValidationError("대화내용 분석 중에 오류가 발생했습니다.") from e
 
 
 # 아래는 테스트용 코드입니다.
--- app/views.py
+++ app/views.py
@@ -1,8 +1,10 @@
+import traceback
 from fastapi import APIRouter, UploadFile, File
 import zipfile
 import csv
 import io
 
+from app.base.utils import send_message
 from app.dto import PeaceAwardOutput
 from app.exceptions import ValidationError
 from app.prompt import get_peace_award
@@ -25,8 +27,9 @@ async def peace_award(file: UploadFile = File(...)) -> PeaceAwardOutput:
                     with zip_ref.open(filename) as f:
                         if filename.endswith(".txt") or filename.endswith(".csv"):
                             text_contents.append(f.read().decode("utf-8"))
-                return get_peace_award("\n".join(text_contents))
+                return await get_peace_award("\n".join(text_contents))
         except Exception as e:
+            send_message(str(traceback.format_exc()))
             raise ValidationError(f"ZIP 파일 처리 중 오류 발생: {e}")
 
     elif file_extension == "csv":
@@ -35,14 +38,19 @@ async def peace_award(file: UploadFile = File(...)) -> PeaceAwardOutput:
             content = await file.read()
             csv_reader = csv.reader(io.StringIO(content.decode("utf-8")))
             rows = [",".join(row) for row in csv_reader]
-            return get_peace_award("\n".join(rows))
+            return await get_peace_award("\n".join(rows))
         except Exception as e:
+            send_message(str(traceback.format_exc()))
             raise ValidationError(f"CSV 파일 처리 중 오류 발생: {e}")
 
     elif file_extension == "txt":
         # TXT 파일 처리
-        content = await file.read()
-        return get_peace_award(content.decode("utf-8"))
+        try:
+            content = await file.read()
+            return await get_peace_award(content.decode("utf-8"))
+        except Exception as e:
+            send_message(str(traceback.format_exc()))
+            raise ValidationError(f"TXT 파일 처리 중 오류 발생: {e}")
 
     else:
         raise ValidationError("지원하지 않는 파일 형식입니다.")
